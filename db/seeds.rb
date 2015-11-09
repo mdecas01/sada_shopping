5b52944c9a52c072b6ce46f086018f308c6f8dd8
Rodrigo <romacas@hotmail.co.uk>
2015-11-09 21:59:01 +0000

    add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!([
+	              {
+	                name: "Folding chair",
+                    description: "Easy to store, folding chair",
+                    price: 10.99,
+                    quantity: 1
+                    image_url: "folding-chair.jpg"
+                  },
+                  {
+                    name: "Coffe table 2",
+                    description: "Strong and easy to assemble",
+                    price: 20.99,
+                    quantity: 1
+                    image_url: "coffe-table.jpg"
+                  },
+                  {
+                  	name: "Office chair",
+                    description: "Resistent and confortable",
+                    price: 37.89,
+                    quantity: 1
+                    
+                  },
+                  {
+                  	name: "New sofa",
+                    description: "Confortable and stilish ",
+                    price: 129.89,
+                    quantity: 1
+                  }
+                ])	
