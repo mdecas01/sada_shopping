465e6a07f86a49cc2672ff6383a0ba2a63f48ca9
Rodrigo <romacas@hotmail.co.uk>
2015-12-07 22:00:44 +0000

    product quantity change

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,11 +4,16 @@ class Cart < ActiveRecord::Base
   belongs_to :user
 
   def add_product_item(product)
+    productItem = Product.find(product.id)
     item = product_items.find_by(product_id: product.id)
     if item
       item.quantity += 1
+      productItem.quantity -= 1
+      productItem.save
     else
       item = product_items.build(product_id: product.id)
+      productItem.quantity -= 1
+      productItem.save
     end
     item
   end	
