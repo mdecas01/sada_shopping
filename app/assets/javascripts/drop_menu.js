8253150638e961139230ec09b49c54eb00353218
Rodrigo <romacas@hotmail.co.uk>
2016-01-05 17:55:38 +0000

    debug file change

--- app/assets/javascripts/drop_menu.js
+++ app/assets/javascripts/drop_menu.js
@@ -1,9 +1,8 @@
-/**
-$(function() {
-  $(document).on('change', function() {
-    $('div#second').append('<%= select(:category, :category2_id, Category.where(level: 1), {}, {:onchange => "alert_me_test()"}) %>');
-    //alert('Changed question');
-  });
-});
 
-**/
+//$(function() {
+  //$(document).on('change', function() {
+  //  $('div#second').append('<%= select(:category, :category2_id, Category.where(level: 1), {}, {:onchange => "alert_me_test()"}) %>');
+    //alert('Changed question');
+ // });
+//});
+
