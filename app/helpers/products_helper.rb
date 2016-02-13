7479dd37153f4a242c7f61f8d98c270eb95239c8
Rodrigo <romacas@hotmail.co.uk>
2016-02-13 18:50:17 +0000

    change product page layout

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -24,7 +24,7 @@ module ProductsHelper
 
   def display_add_button(product)
       if product.available?
-         button_to "Add to cart", product_items_path(product: @product), remote: true, id: "btn", class: "button" 
+         button_to "Add to cart", product_items_path(product: @product), remote: true, class: "button" 
       else
        "Not currently available"
       end 
