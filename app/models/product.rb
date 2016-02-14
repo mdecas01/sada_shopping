e7155b409da0e4f83479988a9e3f05c4fdba7ab7
Rodrigo <romacas@hotmail.co.uk>
2016-02-14 12:27:17 +0000

    change product page layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
     #include Comparable
 
     validates :name, :price, presence: true
-    validates :description, length: { minimum: 10 }
+    validates :description, length: { minimum: 10  }
     validates :price, numericality: { greater_than: 0}
     validates :quantity, numericality: { greater_than_or_equal_to: 0}
     validates :image_url, allow_blank: true, format: {
